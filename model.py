4c197410d33b9dbc07ce2a064342c552de5645d1
gouqi <1249224822@qq.com>
2022-01-12 21:34:58 +0800

    add scheduler and  he init

--- model.py
+++ model.py
@@ -3,13 +3,17 @@ import torch
 def fix_seed(seed = 43):
     np.random.seed(seed)
 class Linear:
-    def __init__(self,input_size,hidden_size):
+    def __init__(self,input_size,hidden_size,is_regular):
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.is_regular = is_regular
         self.w = np.random.randn(input_size,hidden_size)
         self.b = np.random.randn(hidden_size)
         self.grad_w = np.random.randn(input_size,hidden_size)
         self.grad_b = np.random.randn(hidden_size)
     def forward(self,x):
-        return np.matmul(x,self.w) + self.b
+        y = np.matmul(x,self.w) + self.b
+        return y
     def backward(self,x,grad): # x是输入，grad是上一层反向传播回来的梯度
         self.grad_w =  np.matmul(x.T,grad)
         self.grad_b  = np.matmul(grad.T,np.ones(len(x)))
@@ -22,6 +26,17 @@ class RELU:
         return np.where(x < 0, 0, x)
     def backward(self,x,grad):
         return np.where(x>0,1,0) * grad # 这里激活函数是对每个元素求的，因此梯度也对每个元素反向传播即可，是对应元素相乘
+
+class Scheduler:
+    def __init__(self,optimizer,step_size):
+        self.optimizer = optimizer
+        self.step_size =  step_size
+        self.gamma = 0.95
+        self.current_pos = 1
+    def step(self,step):
+        if step // self.step_size > self.current_pos:
+            self.optimizer.learning_rate *= self.gamma
+            self.current_pos += 1
 
 class Optimizer:
     def __init__(self,model,learning_rate):
@@ -41,18 +56,20 @@ class Cross_Entropy:
         x = x - np.expand_dims(np.max(x,1),-1)
         # 进行softmax
         # x = np.exp(x) / np.sum(np.exp(x),1)
-        # self.output = [np.log(x[i,yi]) for i,yi in enumerate(y)]  # 会溢出
-
+        # self.output = [np.log(x[i,yi]) for i,yi in enumerate(y)]  # 这样计算会溢出
         # for i in range(len(y)):
         #     each_sample = []
         #     for j in range(len(x[0])):
         #         each_sample.append((x[i, j] - np.log(np.sum(np.exp(x)[i])))) # 里面的是logp(x)
-        self.output = x - np.expand_dims(np.log(np.sum(np.exp(x),1)),-1)
+
+        ##  将上面的过程 用一行代码 表示出来，其实就是log_sum_exp函数
+        self.output = x - np.expand_dims(np.log(np.sum(np.exp(x),1)),-1)  # 注意，这里的output是logp(x)而不是p(x),因此梯度回传的时候要加上exp
         loss = 0
         for i,yi in enumerate(y):
             loss += -self.output[i,yi] # 要加负号才是损失函数
         loss = loss / len(x)
-        loss2 = critierion(torch.tensor(x,dtype = torch.float),torch.tensor(y,dtype=torch.long))
+        #  可以用torch的交叉熵做比较，发现结果是几乎一致的。
+        # loss2 = critierion(torch.tensor(x,dtype = torch.float),torch.tensor(y,dtype=torch.long))
         return loss / len(x)
     def backward(self): # cross_entropy(带softmax)关于MLP最终输出的梯度
         tmp = np.zeros_like(self.output)
@@ -68,13 +85,20 @@ class MLP:
     2.self.prior,模型的运行顺序（包括激活函数）
     3.self.outputs,从最开始的输入x起到最终输出的前一个，这里需要记录中间输出以便后面算梯度。
     '''
-    def __init__(self,input_size):
-        self.linear1 = Linear(input_size,100)
+    def __init__(self,input_size,initilization = 'rand',is_regular = False):
+        self.linear1 = Linear(input_size,100,is_regular)
         self.relu = RELU()
-        self.linear2 = Linear(100,100)
-        self.linear3 = Linear(100,10)
+        self.linear2 = Linear(100,100,is_regular)
+        self.linear3 = Linear(100,10,is_regular)
         ## 记录参数
         self.layers = [self.linear1,self.linear2,self.linear3] #
+        if initilization == 'He init':
+            last_dim = 2 # 初始化设置2
+            for layer in self.layers:
+                layer.w = np.random.randn(layer.input_size,layer.hidden_size) * np.sqrt(2/last_dim)
+                layer.b = np.random.randn(layer.hidden_size) * np.sqrt(2/last_dim)
+                last_dim = layer.hidden_size
+
     def forward(self,x): ## 这里因为没用计算图，就必须将每步的输出结果保存下来
         o1 = self.linear1.forward(x)
         a1 = self.relu.forward(o1)
--- train.py
+++ train.py
@@ -1,40 +1,78 @@
 import numpy as np
-from model import MLP,Optimizer,Cross_Entropy
+from model import MLP,Optimizer,Cross_Entropy,Scheduler
 from dataset import Dataset,DataLoader
+from matplotlib import pyplot as plt
 import random
 def fix_seed(seed = 43):
     random.seed(seed)
     np.random.seed(seed)
+def plot(baseline_loss,other_loss,other_label = 'He initialization'):
+    fig, ax = plt.subplots()
+    ax.set_title('baseline vs ' + other_label)
+    ax.set_xlabel('epochs')
+    ax.set_ylabel('loss')
+    ax.plot(list(range(len(baseline_loss))),baseline_loss,color='red',label='baseline')
+    ax.plot(list(range(len(other_loss))),other_loss,color='blue',label= other_label)
+    ax.legend()
+    fig.show()
+    fig.savefig('output/baseline vs ' + other_label + '.png')
 
-fix_seed()
-model = MLP(784)
-learning_rate = 1e-3
-optimizer = Optimizer(model,learning_rate=learning_rate)
-critierion = Cross_Entropy()
+class Trainer():
+    def __init__(self,initiliaztion = 'rand',adjust_lr = False):
+        fix_seed()
+        self.model = MLP(784,initilization= initiliaztion)
+        self.learning_rate = 3e-3
+        self.adjust_lr = adjust_lr
+        self.optimizer = Optimizer(self.model,learning_rate=self.learning_rate)
+        if adjust_lr:
+            self.learning_rate = 3e-3
+        self.scheduler = Scheduler(optimizer=self.optimizer,step_size=5000)
+        self.critierion = Cross_Entropy()
+        dataset = Dataset()
+        self.dataloader = DataLoader(dataset, batch_size=32)
+    def train(self):
+        min_loss = float('inf')
+        ret = []
+        step = 0
+        model = self.model
+        dataloader = self.dataloader
+        critierion = self.critierion
+        optimizer = self.optimizer
+        scheduler = self.scheduler
+        epochs = 50
+        for epoch in range(epochs):
+            total_loss = []
+            total_acc = []
+            for data,label in dataloader:
+                data =  data / 255
+                logits = model.forward(data)
+                loss = critierion.forward(logits,label)
+                total_loss.append(loss)
+                grad = critierion.backward()
+                ans = np.argmax(logits,1)
+                acc = sum(ans == label) / (len(data))
+                total_acc.append(acc)
+                model.backward(grad)
+                optimizer.step()
+                if self.adjust_lr:
+                    scheduler.step(step)
+                step += 1
+            ret.append(sum(total_loss) / len(total_loss))
+            print("epoch:%d,step:%d,loss:%.5f,acc:%.5f" % (epoch,step,sum(total_loss) / len(total_loss), sum(total_acc) / len(total_acc)))
+        return ret
+trainer = Trainer()
+baseline_loss = trainer.train()
 
-epochs = 10
-dataset = Dataset()
+# 1. change the init method
+# he_init_trainer = Trainer(initiliaztion='He init')
+# he_init_loss = he_init_trainer.train()
+# plot(baseline_loss,he_init_loss,other_label='He initilization')
 
 
-min_loss = float('inf')
-ans = []
-## x -> (batch_size,input_size)
-## y -> (batch_size,)
-step = 0
-dataloader = DataLoader(dataset, batch_size=16)
-for epoch in range(epochs):
-    total_loss = []
-    total_acc = []
-    for data,label in dataloader:
-        data =  data / 255
-        logits = model.forward(data)
-        loss = critierion.forward(logits,label)
-        total_loss.append(loss)
-        grad = critierion.backward()
-        ans = np.argmax(logits,1)
-        acc = sum(ans == label) / (len(data))
-        total_acc.append(acc)
-        model.backward(grad)
-        optimizer.step()
-        step += 1
-    print("epoch:%d,step:%d,loss:%.5f,acc:%.5f" % (epoch,step,sum(total_loss) / len(total_loss), sum(total_acc) / len(total_acc)))
+# 3. change to  dynamic lr
+dynamic_lr_trainer = Trainer(adjust_lr= True)
+dynamic_lr_loss = dynamic_lr_trainer.train()
+plot(baseline_loss,dynamic_lr_loss,other_label='Dynamic leraning rate')
+
+## 4.  正则项
+
